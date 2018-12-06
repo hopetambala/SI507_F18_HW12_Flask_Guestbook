cc05f5c3648604726e61a5c27f51042f9447d4fa
Hope Tambala <hopetambala@outlook.com>
2018-12-05 20:27:10 -0500

    guestbook

--- app.py
+++ app.py
@@ -13,12 +13,23 @@ def addentry():
     ## add a guestbook entry
     return render_template("addentry.html")
 
+@app.route("/admin")
+def admin():
+    ## add a guestbook entry
+    return render_template("admin.html", entries=model.get_entries())
+
 @app.route("/postentry", methods=["POST"])
 def postentry():
     name = request.form["name"]
     message = request.form["message"]
     model.add_entry(name, message)
     return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    id = request.form["id"]
+    model.delete_entry(id)
+    return redirect("/admin")
 
 if __name__=="__main__":
     model.init()
